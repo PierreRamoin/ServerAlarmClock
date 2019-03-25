b238e28f86b47596b9ae6eb8de2f5895c297cbdd
Pierre RAMOIN <pierre.ramoin@epitech.eu>
2019-03-25 03:37:42 +0100

    [+] Add

--- index.js
+++ index.js
@@ -5,21 +5,46 @@ let app = express();
 
 let db = new sqlite3.Database('./db/alarms.db', sqlite3.OPEN_READWRITE);
 
-
 app.use(bodyParse.json());
 
-app.use('/initDB', function (req, res) {
-   db.run('CREATE TABLE temperatures(id int, date dateTime, temperature float)', (e) => {
-       if (e !== null) {
-           console.log(e);
-           res.status(500).send(e);
-       } else {
-           res.status(200).send('Init ok');
-       }
-   })
+app.use('/db/init', function (req, res) {
+    db.run('CREATE TABLE temperatures(id int, date dateTime, temperature float)', (e) => {
+        if (e !== null) {
+            console.log(e);
+            res.status(500).send(e);
+        } else {
+            res.status(200).send('Init ok');
+        }
+    })
 });
 
-app.get('/lastTemp', )
+app.get('/temp/last', function (req, res) {
+    const params = IDParams(req);
+    db.get("SELECT temperature FROM temperatures "+ params.where + " ORDER BY date DESC", params.args, function (err, result) {
+        if (err == null && result.hasOwnProperty('temperature')) {
+            res.status(200).send(result);
+        } else {
+            res.status(500).send(err)
+        }
+    })
+});
+
+function IDParams(req) {
+    let args = [];
+    let where = "";
+    if (req.query.hasOwnProperty('id')) {
+        where = "WHERE id = ?";
+        args.push(req.query.id);
+    }
+    return {where, args};
+}
+
+app.get('/temp', function (req, res) {
+    const params = IDParams(req);
+    db.all("SELECT date, temperature FROM temperatures " + params.where + " ORDER BY date DESC", params.args, function (err, result) {
+        res.status(200).send(result);
+    });
+});
 
 app.post('/temp', function (req, res) {
     let body = req.body;
